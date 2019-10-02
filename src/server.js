3f8b4fc0820674a4361bd71bf0367d693550c98b
Michael Dauner <mdauner@gmail.com>
2019-10-02 09:24:06 -0500

    Add tailwindcss

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
+import './tailwind.css'
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
